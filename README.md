24456a5fba0a3e5da0673299d27c616aaa0e3511
DcorpProj <dimabobkov2016@gmail.com>
2024-12-03 18:28:03 +0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,5 @@
+### Python Encode
+A simple Python 3.11+ lib for encoding (decoding in 0.0.2)
+Version: python-encode-0.0.1
+### Functions
+encode_base64() - Encodes into Base64 format.
---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+   name='python-encode',
+   version='0.0.1',
+   packages=find_packages(),
+   description='A simple Python encoding library',
+   author='winxp_deb',
+   author_email='dimabobkov2017@gmail.com',
+   url='https://github.com/DcorpProj/python-encode',
+   classifiers=[
+       'Programming Language :: Python :: 3.11',
+       'License :: OSI Approved :: MIT License',
+       'Operating System :: OS Independent',
+   ],
+   python_requires='>=3.11',
+)
