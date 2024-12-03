2be167e9aa6880e2905498ac9a818b362a4e1475
DcorpProj <dimabobkov2016@gmail.com>
2024-12-03 18:32:22 +0300

    Delete encoder.py

--- encoder.py
+++
@@ -1,7 +0,0 @@
-import base64
-
-def encode_base64(text: str) -> str:
-   """Кодирует текст в Base64."""
-   text_bytes = text.encode('utf-8')
-   base64_bytes = base64.b64encode(text_bytes)
-   return base64_bytes.decode('utf-8')
